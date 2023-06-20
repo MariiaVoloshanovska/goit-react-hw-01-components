c26224017a8072334d2f309ffd5c12cc96ff8804
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-06-20 14:44:48 +0300

    1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,27 @@
 
 // import PropTypes from 'prop-types';
 
+// import css from './FriendList.module.css';
+
+// import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+// export const FriendList = ({ friends }) => {
+//   return (
+//     <ul className={css.friendList}>
+//       {friends.map(friend => (
+//         <FriendListItem
+//           key={friend.id}
+//           avatar={friend.avatar}
+//           name={friend.name}
+//           isOnline={friend.isOnline}
+//         />
+//       ))}
+//     </ul>
+//   );
+// }; 
+
+
+import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
@@ -18,4 +39,8 @@ export const FriendList = ({ friends }) => {
       ))}
     </ul>
   );
-}; // What am I doing right now?))
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
