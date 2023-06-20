40259e9ab0be6471d209b2a7f6c2db0f207ba201
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-06-20 16:45:22 +0300

    one more update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,31 +11,31 @@ import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import transactions from 'data/transactions.json';
 
 
-// export const App = () => {
-//   return (
-//     <>
-//       <Profile {...user} />
-//       <Statistics title="Upload stats" stats={data} />
-//       <FriendList friends={friends} />
-//       <TransactionHistory items={transactions} />;
-//     </>
-//   );
-// };
-
-
 export const App = () => {
   return (
     <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <Profile {...user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
     </>
   );
-}; 
+};
+
+
+// export const App = () => {
+//   return (
+//     <>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       <Statistics title="Upload stats" stats={data} />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />;
+//     </>
+//   );
+// }; 
